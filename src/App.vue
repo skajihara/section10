827a498277156e7f67b8cd393a62260ae30c51db
skajihara <t15cs015@gmail.com>
2024-03-29 15:54:33 +0900

    section10-6

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-
-const userInput = ref('')
-const message = ref('')
-const checked = ref('not checked')
-const fruits = ref([])
-const gender = ref('male')
-const selected = ref([])
+import CustomInput from './components/CustomInput.vue'
+const userInput = ref('hello')
+const titleInput = ref('title')
 </script>
 <template>
   <h1>v-model</h1>
-  <h2>Text</h2>
-  <input v-model.lazy.trim.number="userInput" type="number" />
-  <p>{{ typeof userInput }}</p>
-  <h2>Textarea</h2>
-  <textarea v-model.trim="message"></textarea>
-  <p style="white-space: pre">{{ message }}</p>
-  <h2>Checkbox</h2>
-  <input
-    id="checkbox"
-    v-model="checked"
-    type="checkbox"
-    true-value="checked"
-    false-value="not checked"
-  />
-  <label for="checkbox">{{ checked }}</label>
-  <p>Fruits</p>
-  <input id="Apple" v-model="fruits" type="checkbox" value="Apple" />
-  <label for="Apple">Apple</label>
-  <input id="Banana" v-model="fruits" type="checkbox" value="Banana" />
-  <label for="Banana">Banana</label>
-  <input id="Grape" v-model="fruits" type="checkbox" value="Grape" />
-  <label for="Grape">Grape</label>
-  <p>{{ fruits }}</p>
-  <h2>Radio</h2>
-  <input id="male" v-model="gender" type="radio" value="male" />
-  <label for="male">male</label>
-  <input id="female" v-model="gender" type="radio" value="female" />
-  <label for="female">female</label>
-  <p>{{ gender }}</p>
-  <h2>Select</h2>
-  <select v-model="selected" multiple>
-    <option value="" disabled>Select one</option>
-    <option v-for="option in ['A', 'B', 'C']" :key="option">{{ option }}</option>
-  </select>
-  <p>{{ selected }}</p>
+  <CustomInput v-model.uppercase="userInput" v-model:title-name="titleInput" />
+  <p>userInput: {{ userInput }}</p>
+  <p>titleInput: {{ titleInput }}</p>
 </template>
